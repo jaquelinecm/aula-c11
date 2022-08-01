a0a2b367bbb477509e301ea41039301a20e1954c
Jaqueline Couto Moreira <jackcmoreira@gmail.com>
2022-08-01 19:43:37 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+var trex ,trex_running;
+var ground;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //criação do trex
+  trex = createSprite(50,160, 20, 50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+
+  //criação do chão
+  ground = createSprite(200,180,400,20);
+ 
+}
+
+function draw(){
+  background("white");
+
+  if(keyDown("space")){
+    trex.velocityY = -10;
+  }
+  trex.velocityY = trex.velocityY + 0.5;
+
+  trex.collide(ground);
+  drawSprites();
+  
+
+}
